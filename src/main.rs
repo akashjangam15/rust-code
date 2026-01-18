9585b0df216888be4cf3574e32dd81e3f2c5c661
Akash Jangam <akashjangam708@gmail.com>
2026-01-18 14:45:24 +0530

    enum

--- src/main.rs
+++ src/main.rs
@@ -16,21 +16,48 @@
 
 //implementing struct
 
-struct rect{
-    width: u32,
-    height: u32,
+// struct rect{
+//     width: u32,
+//     height: u32,
+// }
+
+// impl rect{
+//     fn area(&self)->u32{
+//         return self.width*self.height;
+//     }
+// }
+
+// fn main(){
+//     let rect1=rect{
+//         width:10,
+//         height:20,
+//     };
+//     println!("{}",rect1.area()); 
+
+// }
+
+enum Shape{
+    Circle(f64),
+    Rectangle(f64,f64),
+    Square(f64),
 }
 
-impl rect{
-    fn area(&self)->u32{
-        return self.width*self.height;
-    }
+fn calculate_area(shape:Shape)->f64{
+    let area=match shape{
+        Shape:: Square(a)=>a*a,
+        Shape::Rectangle(x,b )=>x*b,
+        Shape::Circle(z)=>3.14*z*z,
+    };
+    return area;
 }
 
 fn main(){
-    let rect1=rect{
-        width:10,
-        height:20,
-    };
-    println!("{}",rect1.area());  
+    let circle=Shape::Circle(5.0);
+    let square= Shape::Square(4.0);
+    let rectangel=Shape::Rectangle(3.0,6.0);
+
+    let ans=calculate_area(rectangel);
+    
+    println!("{}",ans);
+
 }
