b54fd5fd5e417a6f2ff484ad23bf9265bfb5cde9
Akash Jangam <akashjangam708@gmail.com>
2026-01-18 11:09:40 +0530

    struct impl

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,36 @@
-use std::string;
+// struct User{
+//     active :bool,
+//     username: String,
+//     age:i32,
+// }
 
-fn get_string_length(s: &str)->usize{
-    return s.chars().count();
+// fn main(){
+//     let user=User{
+//         username: String::from("Akash"),
+//         active:true,
+//         age:21,
+
+//     };
+//     println!("{}",user.username);
+// }
+
+//implementing struct
+
+struct rect{
+    width: u32,
+    height: u32,
+}
+
+impl rect{
+    fn area(&self)->u32{
+        return self.width*self.height;
+    }
 }
 
 fn main(){
-    let name=String::from("Akash");
-    let len=get_string_length(&name);
-
-    println!("length of the string: {:?}",len);
+    let rect1=rect{
+        width:10,
+        height:20,
+    };
+    println!("{}",rect1.area());  
 }
-
