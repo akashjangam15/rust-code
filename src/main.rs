b708e3135118b514d380d48ed1cad18d803b3993
Akash Jangam <akashjangam.ai28@jecrc.ac.in>
2026-01-19 15:40:29 +0530

    vector

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
-fn main(){
-    let mut s1=String::from("Akash");
-    do_someting(&s1);
-    println!("{}",s1);
+use std::vec;
 
+fn main(){
+    let mut vec= Vec::new();
+    vec.push(1);
+    vec.push(2);
+    vec.push(3);
+    println!("{:?}",vec);
+    let even_vec=even_filter(vec);
+    println!("{:?}",even_vec);
 }
-fn do_someting(s2:& String){
-    println!("{}",s2);
+
+fn even_filter(vec:Vec<i32>)->Vec<i32>{
+    let mut new_vec=Vec::new();
+
+    for val in vec{
+        if val%2==0{
+            new_vec.push(val);
+        }
+
+    }
+    return new_vec;
 }
