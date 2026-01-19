8947a587d1845355ccc104a1d24771b9080953ca
Akash Jangam <akashjangam.ai28@jecrc.ac.in>
2026-01-19 15:49:29 +0530

    vec refrence and dereference

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,17 @@ fn main(){
     vec.push(2);
     vec.push(3);
     println!("{:?}",vec);
-    let even_vec=even_filter(vec);
+    let even_vec=even_filter(&vec);
     println!("{:?}",even_vec);
+    println!("{:?}",vec);
 }
 
-fn even_filter(vec:Vec<i32>)->Vec<i32>{
+fn even_filter(vec:&Vec<i32>)->Vec<i32>{
     let mut new_vec=Vec::new();
 
     for val in vec{
         if val%2==0{
-            new_vec.push(val);
+            new_vec.push(*val);
         }
 
     }
