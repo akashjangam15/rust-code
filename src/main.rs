a7f67fc7108c4988a81393c19644ccb939abc04b
Akash Jangam <akashjangam.ai28@jecrc.ac.in>
2026-01-20 13:21:29 +0530

    filter,map

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,20 @@
-//iterators
-
-
-//iterators using loops
-
-use std::vec;
-
-// fn main(){
-//     let v1=vec![1,2,3];
-//     // let v1_iter=v1.iter();
-//     // println!("{:?}",v1_iter);
-
-//     for v in v1.iter(){
-//         print!("{} ",v);
-//     }
-
-//     print!("{:?}",v1);
-// }
-
-
-//iter_mut --- a mutable iterator
-// fn main(){
-//     let mut v1=vec![1,2,3,4];
-
-//     let mut v1_iter=v1.iter_mut();
-
-//     for val in v1_iter{
-//         *val=*val+1;
-//     }
-// }
-
-//iter.next
-// fn main(){
-//     let mut  num=vec![1,2,3];
-//     let mut num_iter=num.iter_mut();
-
-//     let first_num=num_iter.next();
-//     let second_num=num_iter.next();
-//     let third_num=num_iter.next();
-//     let fourth_num=num_iter.next();
-
-//     match first_num{
-//         Some(val)=>println!("{}",val),
-//         None=>println!("Nothing here"),
-//     }
-//       match fourth_num{
-//         Some(val)=>println!("{}",val),
-//         None=>println!("Nothing here"),
-//     }
-
-
-    // while let Some(val)=num_iter.next(){
-    //     print!("{} ",val);
-    // }
-// }
-
-
 fn main(){
-    let mut nums=vec![1,2,3];
-    let mut iter=nums.into_iter();
+    let  v1=vec![1,2,3];
+    let mut v1_iter=v1.iter(); 
+    let v1_iter_x=v1_iter.clone();
 
-    for val in iter{
-        println!("{}",val);
+    // let total:i32=v1_iter.sum();  //consuming adeptor of vi_iter
+    // println!("{}",total);
+    // println!("{:?}",v1);
+
+
+    let v1_iter2=v1_iter.map(|x| x+1);
+    let v1_iter3=v1_iter_x.filter(|x|*x%2==0);
+    for n in v1_iter2{
+        println!("{}",n);
     }
-    // print!("{:?}",nums);
+    for n in v1_iter3{
+        println!("{}",n);
+    }
 
 }
- 
