2c568dd8a507655d773c80312f2dd2863172369d
Akash Jangam <akashjangam.ai28@jecrc.ac.in>
2026-01-21 23:10:38 +0530

    lifetime issue of variable

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,20 @@
-//traits
-
-pub trait Summary {
-    fn summirize(&self){
-        println!("hii i there from summ ");
-    }
-    
-}
-pub trait Fix {
-    fn fix(&self){
-        println!("hii i there from fix ");
-    }
-    
-}
-
-struct User{
-    name:String,
-    age:i32,
-}
-
-impl Summary for User{}
-impl Fix for User{}
-
 fn main(){
-    let user=User{
-        name:String::from("Akash"),
-        age:21,
-    };
-    notify(user);
+    let ans;
+    let str1=String::from("small");
 
+    {
+        let str2=String::from("Largest");
+        ans=longest(&str1,&str2);
+    }
+
+    println!("{}",ans);
 }
 
-fn notify<T: Summary + Fix>(u:T){
-    println!("{:?} {:?}",u.summirize(),u.fix());
+fn longest(a: &str,b: &str)->&str{
+    if a.len()>b.len(){
+        return a;
+    }
+    else{
+        return b;
+    }
 }
