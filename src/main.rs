841f5129195c89b19a990766f56af78cd7e8f808
Akash Jangam <akashjangam.ai28@jecrc.ac.in>
2026-01-21 21:02:23 +0530

    traits

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,27 @@
+//traits
+
+pub trait Summary {
+    fn summirize(&self)->String{
+        return String::from("Summirized");
+    }
+    
+}
+
+struct User{
+    name:String,
+    age:i32,
+}
+
+impl Summary for User{
+    fn summirize(&self)->String {
+        return format!("The name is {} and age is {}",self.name,self.age);
+    }
+}
+
 fn main(){
-    let  v1=vec![1,2,3];
-    let mut v1_iter=v1.iter(); 
-    let v1_iter_x=v1_iter.clone();
-
-    // let total:i32=v1_iter.sum();  //consuming adeptor of vi_iter
-    // println!("{}",total);
-    // println!("{:?}",v1);
-
-
-    let v1_iter2=v1_iter.map(|x| x+1);
-    let v1_iter3=v1_iter_x.filter(|x|*x%2==0);
-    for n in v1_iter2{
-        println!("{}",n);
-    }
-    for n in v1_iter3{
-        println!("{}",n);
-    }
-
+    let user=User{
+        name:String::from("Akash"),
+        age:21,
+    };
+    println!("{}",user.summirize());
 }
