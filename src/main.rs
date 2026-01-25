eff1b51498670a3e3b4f82b66b76894824ff1d4d
Akash Jangam <akashjangam.ai28@jecrc.ac.in>
2026-01-25 18:57:57 +0530

    hashmap: entry,or_insert

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
-#[derive(Debug)]
-enum Diff_type{
-    Intiger(i32),
-    Float(f64),
-    Text(String)
-}
+use std::{collections::HashMap, string};
 
 fn main(){
-    let row=vec![Diff_type::Intiger(4),
-    Diff_type::Float(5.6),
-    Diff_type::Text(String::from("akash"
-    ))];
 
-    println!("{:?}",row);
+    // let mut scores=HashMap::new();
+    // scores.insert(String::from("blue"),10);
+    // scores.insert(String::from("yellow"), 20);
+    // let team_name=String::from("blue");
+
+    // let score=scores.get(&team_name).copied().unwrap_or(0);
+    // println!("{}",score);
+
+    let text="Hii I am Akash and I am a rust Developer.";
+    let mut map=HashMap::new();
+
+    for word in text.split_whitespace(){
+        let count=map.entry(word).or_insert(0);
+        *count+=1;
+    }
+
+    println!("{map:?}");
+
 
 }
